afc4c83adb742a5cb9a5c3e79bfdbc4c12897df9
Xingyu Qu <1744870019@qq.com>
2025-06-20 20:45:55 +0800

    Update contrast.py

--- contrast.py
+++ contrast.py
@@ -5,7 +5,7 @@ from langchain.chat_models import ChatOpenAI
 # 中文输出防乱码
 sys.stdout.reconfigure(encoding='utf-8')
 
-# 设置代理（可选）
+# 设置代理（修改成您的代理）
 os.environ["http_proxy"]  = "http://127.0.0.1:33210"
 os.environ["https_proxy"] = "http://127.0.0.1:33210"
 
